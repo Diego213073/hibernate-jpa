c8eb11ee1ce16c8ae7a33a6ea25438a9d97c6890
Diego Roble <diego.roble@zabud.com.co>
2021-11-16 23:32:44 -0500

    objeto entityManagerFactory para gestión de todas las entidades

--- src/main/java/org/hibernate/app/util/JpaUtilPersistence.java
+++ src/main/java/org/hibernate/app/util/JpaUtilPersistence.java
@@ -1,2 +1,20 @@
-package org.hibernate.app.util;public class JpaUtilPersistence {
+package org.hibernate.app.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaUtilPersistence {
+
+   private static final EntityManagerFactory entityManagerFactory = builderEntityManagerFactory();
+
+
+   public static EntityManagerFactory builderEntityManagerFactory(){
+     return Persistence.createEntityManagerFactory("hibernate-jpa");
+   }
+
+   public static EntityManager getEntityManager(){
+     return entityManagerFactory.createEntityManager();
+   }
+
 }
