41be454605b25711f98805ffa897506921f553e2
Diego Roble <diego.roble@zabud.com.co>
2021-11-16 23:47:57 -0500

    enum de pagos

--- src/main/java/org/hibernate/app/util/Pagos.java
+++ src/main/java/org/hibernate/app/util/Pagos.java
@@ -1,2 +1,17 @@
-package org.hibernate.app.util;public enum Pagos {
+package org.hibernate.app.util;
+
+public enum Pagos {
+
+  DEBITO("debito"),
+  CREDITO("credito");
+
+  private final String value;
+
+  Pagos(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
 }
