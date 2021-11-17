554f57800bf51444499228658a0ecb7705694c06
Diego Roble <diego.roble@zabud.com.co>
2021-11-16 23:50:36 -0500

    entidad de cliente

--- src/main/java/org/hibernate/app/model/Cliente.java
+++ src/main/java/org/hibernate/app/model/Cliente.java
@@ -1,2 +1,37 @@
-package org.hibernate.app.model;public class Cliente {
+package org.hibernate.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.app.util.Pagos;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private String nombre;
+
+  private String apellido;
+
+  @Column(name = "email")
+  private String correo;
+
+  @Column(name = "forma_pago")
+  private Pagos metodoPago;
+
 }
