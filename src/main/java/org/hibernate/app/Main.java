de860febbf9126b93579e8ddbfca7898693357fe
Diego Roble <diego.roble@zabud.com.co>
2021-11-17 23:27:47 -0500

    persistir registro en base de datos

--- src/main/java/org/hibernate/app/Main.java
+++ src/main/java/org/hibernate/app/Main.java
@@ -1,10 +1,32 @@
 package org.hibernate.app;
 
+import jakarta.persistence.EntityManager;
+import org.hibernate.app.model.Cliente;
+import org.hibernate.app.model.Factura;
+import org.hibernate.app.util.JpaUtilPersistence;
+import org.hibernate.app.util.Pagos;
+
 public class Main {
 
   public static void main(String arg[]) {
 
-    System.out.println("Hola mundo!");
+    EntityManager entityManager = JpaUtilPersistence.getEntityManager();
+
+    try {
+      entityManager.getTransaction().begin();
+      Cliente cliente = Cliente.builder().nombre("Diego").apellido("Alejandro").metodoPago(Pagos.CREDITO.getValue())
+          .correo("diegorobles@gamil.com").build();
+      entityManager.persist(cliente);
+      Factura factura = Factura.builder().descripcion("Xioami Redmit note pro").cliente(cliente).total(5000L)
+          .build();
+      entityManager.persist(factura);
+      entityManager.getTransaction().commit();
+    } catch (Exception e) {
+      entityManager.getTransaction().rollback();
+      System.out.println(e.getMessage());
+    } finally {
+      entityManager.close();
+    }
 
   }
 
