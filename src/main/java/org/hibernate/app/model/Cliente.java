8a8d713843ab9817cc264498f2c5b681f4e7be62
Diego Roble <diego.roble@zabud.com.co>
2021-11-17 23:26:58 -0500

    modificado tipo de dato forma pago

--- src/main/java/org/hibernate/app/model/Cliente.java
+++ src/main/java/org/hibernate/app/model/Cliente.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ import org.hibernate.app.util.Pagos;
 @Setter
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "clientes")
 public class Cliente {
@@ -32,6 +34,6 @@ public class Cliente {
   private String correo;
 
   @Column(name = "forma_pago")
-  private Pagos metodoPago;
+  private String metodoPago;
 
 }
