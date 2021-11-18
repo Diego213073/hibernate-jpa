5946f3dd5d6c2d4fa8258929a8698c1e99869a86
Diego Roble <diego.roble@zabud.com.co>
2021-11-18 00:58:56 -0500

    entidad dirección creada

--- src/main/java/org/hibernate/app/model/Direccion.java
+++ src/main/java/org/hibernate/app/model/Direccion.java
@@ -1,2 +1,31 @@
-package org.hibernate.app.model;public class Direccion {
+package org.hibernate.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "direcciones")
+public class Direccion {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String calle;
+  private String numero;
+
+
+
 }
