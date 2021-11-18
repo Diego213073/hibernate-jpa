b02eee72d869610781fbd6548c8da714f5b5a79b
Diego Roble <diego.roble@zabud.com.co>
2021-11-18 00:58:12 -0500

    anotaciones para obtener en objeto y sus hijos

--- src/main/java/org/hibernate/app/model/Cliente.java
+++ src/main/java/org/hibernate/app/model/Cliente.java
@@ -1,23 +1,28 @@
 package org.hibernate.app.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.app.util.Pagos;
+import lombok.ToString;
 
+@ToString(exclude = "Direccion")
+@EqualsAndHashCode(exclude = "Direccion")
 @Getter
 @Setter
-@Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "clientes")
 public class Cliente {
@@ -35,5 +40,16 @@ public class Cliente {
 
   @Column(name = "forma_pago")
   private String metodoPago;
+
+  //CascadeType.ALL da la ventaja de no
+  // crear otro método para persistir la información en la base de datos. Lo hace automático
+  //Crea tabla intermedia cuando se maneja la llave foranea automaticamente
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name ="id_cliente")
+  private List<Direccion> direccionList;
+
+
+  public Cliente() {
+  }
 
 }
