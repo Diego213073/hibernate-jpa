b35083b2266fa91038e32b6da25365e27c7d1a30
Diego Roble <diego.roble@zabud.com.co>
2021-11-21 17:55:10 -0500

    relación uno a muchos

--- src/main/java/org/hibernate/app/model/Cliente.java
+++ src/main/java/org/hibernate/app/model/Cliente.java
@@ -2,30 +2,29 @@ package org.hibernate.app.model;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.util.ArrayList;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
-@ToString(exclude = "Direccion")
-@EqualsAndHashCode(exclude = "Direccion")
+
 @Getter
 @Setter
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "clientes")
+
 public class Cliente {
+
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,15 +40,39 @@ public class Cliente {
   @Column(name = "forma_pago")
   private String metodoPago;
 
-  //CascadeType.ALL da la ventaja de no
-  // crear otro método para persistir la información en la base de datos. Lo hace automático
-  //Crea tabla intermedia cuando se maneja la llave foranea automaticamente
   @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-  @JoinColumn(name ="id_cliente")
+  @JoinTable(name = "clientes_direcciones", joinColumns = @JoinColumn(name = "id_cliente"),
+      inverseJoinColumns = @JoinColumn(name = "id_direccion"),
+      uniqueConstraints = @UniqueConstraint(columnNames = {
+          "id_direccion"}))
   private List<Direccion> direccionList;
+
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "cliente")
+  private List<Factura> facturaList;
+
+
+  @Embedded
+  private GenerarFechas generarFechas;
 
 
   public Cliente() {
+    direccionList = new ArrayList<>();
+    facturaList = new ArrayList<>();
+  }
+
+  public Cliente(String nombre, String apellido, String correo, String metodoPago,
+      GenerarFechas generarFechas) {
+    this();
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.correo = correo;
+    this.metodoPago = metodoPago;
+    this.generarFechas = generarFechas;
+  }
+
+  public void addFactura(Factura factura) {
+    this.facturaList.add(factura);
+    factura.setCliente(this);
   }
 
 }
---
+++ src/main/java/org/hibernate/app/model/GenerarFechas.java
@@ -0,0 +1,31 @@
+package org.hibernate.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+@Embeddable
+public class FechaCreacion {
+
+  @Column(name ="fecha_creacion")
+  private LocalDateTime fechaCreacion;
+
+  @Column(name ="fecha_modificacion")
+  private LocalDateTime fechaModificacion;
+
+
+  @PrePersist
+  public void insertFechaCreacion(){
+    fechaCreacion = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  public void insertFechaModifiacion(){
+    fechaModificacion = LocalDateTime.now();
+  }
+
+}
