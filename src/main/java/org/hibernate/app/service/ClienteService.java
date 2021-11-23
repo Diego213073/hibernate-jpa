9a0a040cafd7de90e3004980fe5711c7c505b677
Diego Roble <diego.roble@zabud.com.co>
2021-11-23 02:37:34 -0500

    servicios

--- src/main/java/org/hibernate/app/service/ClienteService.java
+++ src/main/java/org/hibernate/app/service/ClienteService.java
@@ -1,2 +1,17 @@
-package org.hibernate.app.service;public interface ClienteService {
+package org.hibernate.app.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.hibernate.app.model.Cliente;
+
+public interface ClienteService {
+
+  List<Cliente> findAll();
+
+  void save(Cliente cliente);
+
+  void deleteById(Integer id);
+
+  Optional<Cliente> get(Integer id);
+
 }
--- src/main/java/org/hibernate/app/service/impl/ClienteServiceImplement.java
+++ src/main/java/org/hibernate/app/service/impl/ClienteServiceImplement.java
@@ -1,2 +1,48 @@
-package org.hibernate.app.service;public class ClienteServiceImplement {
+package org.hibernate.app.service.impl;
+
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import org.hibernate.app.model.Cliente;
+import org.hibernate.app.repository.ClienteRepository;
+
+public class ClienteServiceImplement implements ClienteRepository {
+
+  private EntityManager entityManager;
+
+  public ClienteServiceImplement(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
+
+  @Override
+  public List<Cliente> findAll() {
+    return entityManager.createQuery("select c from Cliente c", Cliente.class).getResultList();
+  }
+
+  @Override
+  public void save(Cliente t) {
+    if (Objects.isNull(t.getId())) {
+      entityManager.getTransaction().begin();
+      entityManager.persist(t);
+      entityManager.getTransaction().commit();
+    }else{
+      entityManager.getTransaction().begin();
+      entityManager.merge(t);
+      entityManager.getTransaction().commit();
+    }
+  }
+
+  @Override
+  public void deleteById(Integer id) {
+    Cliente cliente = get(id).orElseThrow(() -> new RuntimeException("El cliente no se encuentra en BD"));
+    entityManager.getTransaction().begin();
+    entityManager.remove(cliente);
+    entityManager.getTransaction().commit();
+  }
+
+  @Override
+  public Optional<Cliente> get(Integer id) {
+    return Optional.ofNullable(entityManager.find(Cliente.class, id));
+  }
 }
