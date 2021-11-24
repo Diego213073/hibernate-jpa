a3bcaf6870534401cc7076fe74a25c5dd66df3d7
Diego Roble <diego.roble@zabud.com.co>
2021-11-24 01:20:21 -0500

    agregar cliente con todas sus relaciones

--- src/main/java/org/hibernate/app/Main.java
+++ src/main/java/org/hibernate/app/Main.java
@@ -1,6 +1,8 @@
 package org.hibernate.app;
 
 import jakarta.persistence.EntityManager;
+import java.util.Arrays;
+import org.hibernate.app.model.Categoria;
 import org.hibernate.app.model.Cliente;
 import org.hibernate.app.model.ClienteDetalle;
 import org.hibernate.app.model.Direccion;
@@ -19,14 +21,33 @@ public class Main {
 
       ClienteServiceImplement clienteServiceImplement = new ClienteServiceImplement(entityManager);
 
+      Categoria categorias = Categoria.builder().descripcion("Entretenimiento").puntos(2000).build();
+      Categoria categorias2 = Categoria.builder().descripcion("Calculo").puntos(2900).build();
+      Categoria categorias3 = Categoria.builder().descripcion("Diversion").puntos(2500).build();
+
+
+
       Cliente cliente = new Cliente("Carlos", "Revolledo", "carlosrevo202@gmail.com",
           Pagos.CREDITO.getValue());
 
-      cliente.addDreccion(Direccion.builder().calle("Permitral").numero("calle 64#").build());
-      cliente.addClienteDetalle(new ClienteDetalle(5000));
-      cliente.addFactura(new Factura("Teclado mecánico", 4300L));
+      Cliente cliente2 = new Cliente("Fabiano", "Caruana", "fabiCarun@gmail.com",
+          Pagos.CREDITO.getValue());
 
-      clienteServiceImplement.save(cliente);
+      Cliente cliente3 = new Cliente("Magnus", "Carlsen", "magnusC@gmail.com",
+          Pagos.CREDITO.getValue());
+
+      Arrays.asList(cliente,cliente2,cliente3)
+      .stream().map(c ->{
+        c.addDreccion(Direccion.builder().calle("Permitral").numero("calle 64#").build());
+        c.addFactura(new Factura("Teclado mecánico", 4300L));
+        Arrays.asList(categorias, categorias2, categorias3).forEach(c::addCategorias);
+        int puntosAcumulados = cliente.getCategoriasList().stream().map(Categoria::getPuntos)
+            .reduce(Integer::sum).get();
+        c.addClienteDetalle(new ClienteDetalle(puntosAcumulados
+        ));
+        return c;
+      }).forEach(clienteServiceImplement::save);
+
 
     } catch (Exception e) {
       entityManager.getTransaction().rollback();
@@ -36,7 +57,6 @@ public class Main {
     }
 
   }
-
 
 
 }
